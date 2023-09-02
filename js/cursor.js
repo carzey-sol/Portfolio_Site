1e78ee2126a59fc5b9e15c1165af5f6526755256
Mukesh Bajgain <112241933+carzey-sol@users.noreply.github.com>
2023-09-02 20:04:53 +0545

    123

--- js/cursor.js
+++
@@ -1,6 +0,0 @@
-
-document.addEventListener('mousemove', (e) => {
-    var cursor = document.querySelector('.cursor');
-    cursor.style.left = event.clientX + 'px';
-    cursor.style.top = event.clientY + 'px';
-})
